76159776745c68e28122158f895fc43a524388a9
SlicedorDiced <Joshua.luna-perez@bryanuniversity.edu>
2022-04-18 23:17:54 -0400

    week3 update

--- week3/assignments/gradeCurver/gradeCurver.js
+++ week3/assignments/gradeCurver/gradeCurver.js
@@ -1,8 +1,19 @@
 // declare each of the variables marked with "**" in the appropriate scope and using the appropriate type
 
+let mean = 0;
+let gradeSlice = 0;
+
 // create an event listener that calls the curveGrade() function when the Curve It!! button is clicked
+  document.querySelector('#submit').addEventListener('click', curveGrades);
+   
 
 // create an event listener that resets the scores and grades to their defaults when the Reset button is clicked
+ document.querySelector('#reset').addEventListener('click', () => {
+        document.querySelector('#grades').value = "";
+        document.querySelector('#scores').value = "";
+    } );
+
+
 
 function applyBell(grade, index, ary) {
     switch (true) {
@@ -24,12 +35,6 @@ function applyBell(grade, index, ary) {
     }
 }
 
-    document.querySelector('#submit').addEventListener('click', curveGrades());
-    document.querySelector('#reset').addEventListener('click', () => {
-        document.querySelector('#grades').value = "";
-        document.querySelector('#scores').value = "";
-    } );
-
 function convertArray(obj) {
     ary = obj.value.split(',')
     ary = ary.map(function (x) {
@@ -43,28 +48,35 @@ function convertArray(obj) {
 // separate lines of code into single lines. It currently has 18 lines of code. Without counting  
 // empty lines, can you get the number of lines down to 8?
 
-const curveGrades = (accumulator, currentValue) => accumulator + currentValue
+function curveGrades() {
     // **sum = function (accumulator, currentValue) {
         //return accumulator + currentValue
     //}
+    
+    //const sum = (accumulator, currentValue) => accumulator + currentValue // instead of assigning  all of this code we moved the function its self insde reduce()
 
-    var sumGrades = (array) => array.reduce(sum)
+    var sumGrades = (array) => array.reduce((accumulator, currentValue) => accumulator + currentValue)
 
-    var aryGrades = convertArray(document.querySelector('scores'))
+    var aryGrades = convertArray(document.querySelector('#scores'))
 
     const minGrades = aryGrades.reduce( (a, b) => Math.min(a, b) )
 
     const maxGrades = aryGrades.reduce( (a, b) => Math.max(a, b) )
 
-    var mean = sumGrades(aryGrades) / aryGrades.length
+    mean = sumGrades(aryGrades) / aryGrades.length;
 
-    var range = maxGrades - minGrades
+    // var range = maxGrades - minGrades
 
-    gradeSlice = range / 5
+    gradeSlice = (maxGrades - minGrades) / 5;
 
-    aryGrades.forEaCH(applyBell)
+    aryGrades.forEach(applyBell)
 
     // write the value of aryGrades to the grades div in the HTML document
 
 
-document.querySelector('grades').innerHTML += aryGrades.value
+document.querySelector('#grades').innerHTML = aryGrades.toLocaleString()
+
+}
+
+//input = 66,72,100,88,92,74,72,94,80,78
+//output = F,F,A,C,B,F,F,B,D,D
--- week3/exercises/moviesArrow.js
+++ week3/exercises/moviesArrow.js
@@ -1,4 +1,4 @@
-const movies = [ 
+const movies = [
     { name: 'Avatar', year: 2009, gross: 2847246203 },
     { name: 'Endgame', year: 2019, gross: 2797501328 },
     { name: 'Titanic', year: 1997, gross: 2201647264 },
@@ -10,46 +10,127 @@ const movies = [
     { name: 'Furious 7', year: 2015, gross: 1515255622 },
     { name: 'Frozen II', year: 2019, gross: 1450026933 }
 ]
+// print list of top 10 movies
+//declared function
+// let obj = document.querySelector('#top10Movies')
+// let top10Movies = '<ol>'
+// movies.forEach(function(movie) {
+//     top10Movies += `<li>${movie.name} (${movie.year}) :$${movie.gross.toLocaleString()} </li> `
+// })
+// top10Movies += '</ol>'
+// document.querySelector('#top10Movies').innerHTML = top10Movies
+// arrow function
+let obj = document.querySelector('#top10Movies')
+movies.forEach( movie => updateDOM(`<li>${movie.name} (${movie.year}): $${movie.gross.toLocaleString()} </li> `, true ))
+//              -----------NEXT---------------
+// let movies2B = '<ol>'
+// ary2BMovies.forEach(function(movie ){
+//     movies2B += `<li>${movie.name} (${movie.year}): $${movie.gross.toLocaleString()}</li>`
+// })
+// movies2B += '</ol>'
+// document.querySelector('#movies2B').innerHTML = movies2B
+// - - - - - - - - -  - - NEXT -- - - - - - - -
+//arrow function
+obj = document.querySelector('#movies2B')
+const ary2BMovies = movies.filter( movie => movie.gross >= 2000000000 )
+ary2BMovies.forEach( movie => updateDOM(`<li>${movie.name} (${movie.year}): $${movie.gross.toLocaleString()}</li>`, true))
+// const moviesDTL  = movies.forEach(function(movie) {
+//
+// })
+// const movieNames = movies.map(function(item) {
+//     return item.name
+// })
+//declared
+// let movieNames = '<ol>'
+// movies.map(function(movie){
+//     movieNames += `<li>${movie.name}</li>`
+// }).sort()
+// movieNames += '</ol>'
+// document.querySelector('#alphaMovies').innerHTML = movieNames
+// arrow
+obj = document.querySelector('#alphaMovies') // here with this line in all above what we do is assign the alphamMovies object to the obj variable
+movies.map(movie => updateDOM(`<li>${movie.name}</li>`, true)).sort()
+// - - - - - - - -  - - - NEXT - - - - - - - - -  -
+// const faveMovie = movies.find(function(item) {
+//     return item.name === 'The Lion King'
+// })
+//declared
+// const faveMovie = movies.find(function(item){
+//     return item.id === 'RBWPX'
+// })
+// document.querySelector('#faveMovie').innerHTML = faveMovie.name
+// arrow
+obj = document.querySelector('#faveMovie')
+const faveMovie = movies.find(item => item.id === 'RBWPX')
+updateDOM(faveMovie.name, false)
+// -------------- NEXT - -- -- - - - - - - - - - -
+// // declared
+// const oldMovies = movies.some(function(item) {
+//     return item.year <= 2000
+// })
+// if (oldMovies) {
+//     document.querySelector('#oldMovies').innerHTML = "No"
+// } else {
+//     document.querySelector('#oldMovies').innerHTML = "Yes"
+// }
+// arrow
+obj = document.querySelector('#oldMovies')
+const oldMovies = movies.some(item => item.year <= 2000)
+if (oldMovies) updateDOM('NO', false)
+else updateDOM('Yes', false)
+// - - - - - - - - - - NEXT - - - - - - - - - - - - -
+// const blockBusters = movies.every(function(item) {
+//     return item.gross >= 1500000000
+// })
+// if (blockBusters) {
+//     document.querySelector('#blockbusters').innerText = 'Yes'
+// } else {
+//     document.querySelector('#blockbusters').innerText = 'No'
+// }
+// arrow
+obj = document.querySelector('#blockbusters')
+const blockBusters = movies.every( item => item.gross >= 2000000000)
+if (blockBusters) updateDOM('Yes', false)
+else updateDOM('NO', false)
+// - - - - - - - - - -  NEXT  - - - - - - - - -  -
+    // declared
+// const totalGross = movies.reduce(function(currentTotal, item) {
+//             return item.gross + currentTotal
+//     },
+// 0)
+// document.querySelector('#avgGross').innerText = `$${(totalGross / movies.length).toLocaleString()}`
+// arrow
+obj = document.querySelector('#avgGross')
+const totalGross = movies.reduce( (currentTotal, movie) => movie.gross + currentTotal, 0)
+updateDOM(`$${(totalGross / movies.length).toLocaleString()}`, false)
+// function taking care of inputing new results into the dom
+// obj is the object we refer to
+function updateDOM(val, concat){
+    if(concat) obj.innerHTML += val
+    else obj.innerHTML = val
+}
 
-const movies2B = movies.filter(function(item) {
-    return item.gross >= 2000000000
-})
 
-console.log(movies2B)
 
-const moviesDTL  = movies.forEach(function(movie) {
-    console.log(movie)
-})
 
-console.log(moviesDTL)
 
-const movieNames = movies.map(function(item) {
-    return item.name
-})
 
-console.log(movieNames)
 
-const faveMovie = movies.find(function(item) {
-    return item.name === 'The Lion King'
-})
 
-console.log(faveMovie)
 
-const oldMovies = movies.some(function(item) {
-    return item.year <= 2000
-})
 
-console.log(oldMovies)
 
-const blockBusters = movies.every(function(item) {
-    return item.gross >= 1500000000
-})
 
-console.log(blockBusters)
 
-const totalGross = movies.reduce(function(currentTotal, item) {
-            return item.gross + currentTotal
-    }, 
-0)
-    
-console.log(totalGross / movies.length)
+
+
+
+Message Slackbot
+
+
+
+
+
+
+
+
